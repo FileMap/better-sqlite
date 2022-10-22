450ecba7e73d616858b54123501aae727c232013
Ozan Akın <ozn.akn@gmail.com>
2022-10-22 16:46:51 +0300

    bump version to 5.4.2

--- dist/BetterSqliteDriver.d.ts
+++ dist/BetterSqliteDriver.d.ts
@@ -1,7 +1,7 @@
+import type { Configuration, EntityDictionary, NativeInsertUpdateManyOptions, QueryResult } from '@mikro-orm/core';
 import { AbstractSqlDriver } from '@mikro-orm/knex';
 import { BetterSqliteConnection } from './BetterSqliteConnection';
-import type { AnyEntity, Configuration, EntityDictionary, NativeInsertUpdateManyOptions, QueryResult } from '@mikro-orm/core';
 export declare class BetterSqliteDriver extends AbstractSqlDriver<BetterSqliteConnection> {
     constructor(config: Configuration);
-    nativeInsertMany<T extends AnyEntity<T>>(entityName: string, data: EntityDictionary<T>[], options?: NativeInsertUpdateManyOptions<T>): Promise<QueryResult<T>>;
+    nativeInsertMany<T extends object>(entityName: string, data: EntityDictionary<T>[], options?: NativeInsertUpdateManyOptions<T>): Promise<QueryResult<T>>;
 }
--- dist/BetterSqliteSchemaHelper.d.ts
+++ dist/BetterSqliteSchemaHelper.d.ts
@@ -1,21 +1,21 @@
-import { SchemaHelper } from '@mikro-orm/knex';
 import type { Connection, Dictionary } from '@mikro-orm/core';
-import type { AbstractSqlConnection, Check, Index } from '@mikro-orm/knex';
+import type { AbstractSqlConnection, Index, Check } from '@mikro-orm/knex';
+import { SchemaHelper } from '@mikro-orm/knex';
 export declare class BetterSqliteSchemaHelper extends SchemaHelper {
     disableForeignKeysSQL(): string;
     enableForeignKeysSQL(): string;
     supportsSchemaConstraints(): boolean;
     getListTablesSQL(): string;
-    getColumns(connection: AbstractSqlConnection, tableName: string, _schemaName?: string): Promise<any[]>;
-    getEnumDefinitions(connection: AbstractSqlConnection, _checks: Check[], tableName: string, _schemaName: string): Promise<Dictionary<string[]>>;
-    getPrimaryKeys(connection: AbstractSqlConnection, _indexes: Dictionary, tableName: string, _schemaName?: string): Promise<string[]>;
-    getIndexes(connection: AbstractSqlConnection, tableName: string, _schemaName?: string): Promise<Index[]>;
-    getChecks(_connection: AbstractSqlConnection, _tableName: string, _schemaName?: string): Promise<Check[]>;
-    etForeignKeysSQL(tableName: string): string;
+    getColumns(connection: AbstractSqlConnection, tableName: string, schemaName?: string): Promise<any[]>;
+    getEnumDefinitions(connection: AbstractSqlConnection, checks: Check[], tableName: string, schemaName: string): Promise<Dictionary<string[]>>;
+    getPrimaryKeys(connection: AbstractSqlConnection, indexes: Dictionary, tableName: string, schemaName?: string): Promise<string[]>;
+    getIndexes(connection: AbstractSqlConnection, tableName: string, schemaName?: string): Promise<Index[]>;
+    getChecks(connection: AbstractSqlConnection, tableName: string, schemaName?: string): Promise<Check[]>;
+    getForeignKeysSQL(tableName: string): string;
     mapForeignKeys(fks: any[], tableName: string): Dictionary;
-    databaseExists(_connection: Connection, _name: string): Promise<boolean>;
+    databaseExists(connection: Connection, name: string): Promise<boolean>;
     /**
-   * Implicit indexes will be ignored when diffing
-   */
+     * Implicit indexes will be ignored when diffing
+     */
     isImplicitIndex(name: string): boolean;
 }
